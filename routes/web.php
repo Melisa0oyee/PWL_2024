7b656cb7313f9d59800d90b541b14ed81a50137c
Nur Melisa <nur0melisaoyee@gmail.com>
2024-09-04 09:09:07 +0700

    Praktikum 1 (Pertemuan2)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,50 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//praktikum 1 (2)
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+//praktikum 1 (4)
+Route::get('/world', function () {
+    return 'World';
+});
+
+//praktikum 1 (6)
+Route::get('/ucapan', function () {
+    return 'Selamat Datanng';
+});
+
+//praktikum 1 (7)
+Route::get('/nim', function () {
+    return '2241760029';
+});
+
+//praktikum 1 (8)
+Route::get('/user/{Melisa}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+//praktikum 1 (11)
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+    return 'Pos ke-' .$postId." Komentar ke-: ".$commentId;
+});
+
+//praktikum 1 (13)
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+// praktikum 1 (14)
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+//praktikum 1 (17)
+Route::get('/user/{name?}', function ($name='Nur Melisa') {
+    return 'Nama saya '.$name;
+});
+
